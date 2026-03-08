ace12758c7048fac788a0060bb14bb873845f82b
xiez <xiez1989@gmail.com>
2026-03-08 19:38:38 +0800

    feat: match container uid/gid with host

--- start.sh
+++ start.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
+set -e
 
 dc_base="$(docker compose version &>/dev/null && echo 'docker compose' || echo 'docker-compose')"
 dc="$dc_base"
+
+# Export host UID/GID
+export PUID=$(id -u)
+export PGID=$(id -g)
 
 if [[ -f "../.env" ]]; then
     $dc --env-file .env --env-file ../.env up -d
