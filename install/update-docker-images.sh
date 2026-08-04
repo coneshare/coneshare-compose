4174f8182166ce7f1fa37d0d1987750268683beb
xiez <xiez1989@gmail.com>
2026-08-04 12:34:43 +0800

    feat: add mcp server

--- install/update-docker-images.sh
+++ install/update-docker-images.sh
@@ -3,4 +3,9 @@ echo "${_group}Fetching and updating Docker images ..."
 echo "CONESHARE IMAGE: ${CONESHARE_IMAGE}"
 docker pull ${CONESHARE_IMAGE}
 
+if [[ -n "${CONESHARE_MCP_IMAGE}" ]]; then
+  echo "CONESHARE MCP IMAGE: ${CONESHARE_MCP_IMAGE}"
+  docker pull ${CONESHARE_MCP_IMAGE}
+fi
+
 echo "${_endgroup}"
